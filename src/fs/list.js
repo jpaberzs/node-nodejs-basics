19d49cb64b56b3b757cf45ed284bb494842253c1
jpaberzs <janis.paberzs18@gmail.com>
2024-10-02 00:43:41 +0300

    feat: implemented "read.js" functionality

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,4 +1,4 @@
-import { readdir } from "node:fs/promises";
+import { readdir, access } from "node:fs/promises";
 
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
@@ -11,6 +11,12 @@ const folder = join(__dirname, "files");
 
 const list = async () => {
   try {
+    const isExisting = await access(folder)
+      .then(() => true)
+      .catch(() => false);
+
+    if (!isExisting) throw Error("FS operation failed");
+
     const allFiles = await readdir(folder);
 
     allFiles.forEach((file) => {
