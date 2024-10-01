53da61ebb55daff982e344379972294f00ac0093
jpaberzs <janis.paberzs18@gmail.com>
2024-10-01 12:44:56 +0300

    feat: complected task "create.js"

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import fs from "node:fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const file = "fresh.txt";
+const fileContent = "I am fresh and young";
+
 const create = async () => {
-    // Write your code here 
+  try {
+    if (fs.existsSync(__dirname + `/files/${file}`)) throw new Error("FS operation failed");
+    fs.writeFileSync(__dirname + `/files/${file}`, fileContent);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await create();
