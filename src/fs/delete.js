919fcf437ec2f91fbdd79d370289fe1760771565
jpaberzs <janis.paberzs18@gmail.com>
2024-10-01 23:53:52 +0300

    feat: implemented "delete.js" functionality

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,26 @@
+import { access, unlink } from "node:fs/promises";
+
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+import { styleText } from "node:util";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const file = join(__dirname, "files", "fileToRemove.txt");
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    const isFileExist = await access(file)
+      .then(() => true)
+      .catch(() => false);
+
+    if (!isFileExist) throw new Error("FS operation failed");
+
+    await unlink(file);
+  } catch (error) {
+    process.stderr.write(styleText("red", error + "\n"));
+  }
 };
 
 await remove();
