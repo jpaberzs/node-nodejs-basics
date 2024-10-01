639e8d14e442547bf5f6fcb4f75c2ab52ba2caf1
jpaberzs <janis.paberzs18@gmail.com>
2024-10-01 16:22:53 +0300

    feat: implemented "copy.js" functionality

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,17 +1,39 @@
-import fs from "node:fs";
-import { fileURLToPath } from "url";
+import { cp, access } from "node:fs/promises";
+
 import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+import { styleText } from "node:util";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const folderName = "files_copy";
+const folderStartName = "/files";
+const folderDesctinationName = "/files_copy";
+
+const checkExistingPath = async (folderName) => {
+  try {
+    return await access(__dirname + folderName)
+      .then(() => true)
+      .catch(() => false);
+  } catch (error) {
+    process.stderr.write(styleText("red", error + "\n"));
+  }
+};
 
 const copy = async () => {
   try {
-    fs.mkdirSync(__dirname);
+    const isStartFolder = await checkExistingPath(folderStartName);
+    const isDesctinationFolder = await checkExistingPath(folderDesctinationName);
+
+    if (!isStartFolder || isDesctinationFolder) throw new Error("FS operation failed");
+
+    await cp(__dirname + folderStartName, __dirname + folderDesctinationName, {
+      recursive: true,
+      errorOnExist: true,
+    });
   } catch (error) {
-    throw new Error(error).message;
+    process.stderr.write(styleText("red", error + "\n"));
   }
 };
 
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,6 +1,10 @@
-import fs from "node:fs";
+import { constants } from "node:fs";
+import { access, writeFile } from "node:fs/promises";
+
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+
+import { styleText } from "node:util";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,10 +15,15 @@ const resultFile = __dirname + `/files/${file}`;
 
 const create = async () => {
   try {
-    if (fs.existsSync(resultFile)) throw new Error("FS operation failed").message;
-    return fs.writeFileSync(resultFile, fileContent);
+    const isAccesed = await access(resultFile, constants.F_OK)
+      .then(() => true)
+      .catch(() => false);
+
+    if (isAccesed) throw new Error("FS operation failed");
+
+    await writeFile(resultFile, fileContent);
   } catch (error) {
-    process.stderr.write(error + "\n");
+    process.stderr.write(styleText("red", error + "\n"));
   }
 };
 
